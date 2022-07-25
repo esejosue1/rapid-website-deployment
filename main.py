5254973f4eafc6ef45a5015d5183b1affe271854
Josue Gallardo <69179376+esejosue1@users.noreply.github.com>
2022-07-25 04:09:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st #used to deploy apps in ashort period of time
+
+st.title("Creating a website using streamlit")
+st.subheader('Demonstration of how quick it is to deploy an app')
+st.write("We will be using streamlit in order to deploy our website faster in the cloud.")
