bfb5ba2ba623bc4195eec88369e6932cc23f890a
Josue Gallardo <69179376+esejosue1@users.noreply.github.com>
2022-07-25 04:21:54 +0000

    Adding pandas to import data diagrams

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st #used to deploy apps in ashort period of time
+import pandas #create quick data diagram
+
+data={
+  'Series_1':[1,2,3,4,5],
+  'Series_2':[10,20,30,40,50]
+}
+
+df=pandas.DataFrame(data)
 
 st.title("Creating a website using streamlit")
 st.subheader('Demonstration of how quick it is to deploy an app')
 st.write("We will be using streamlit in order to deploy our website faster in the cloud.")
+st.write(df)
+st.line_chart(df)
